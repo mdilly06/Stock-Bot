1f555099a1771d3da4f879bd030e804f15268644
chanzer0 <seansailer28@gmail.com>
2017-04-09 15:00:48 -0500

    Updating from .xlsx to .csv

--- main.go
+++ main.go
@@ -5,16 +5,14 @@ import (
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/tealeg/xlsx"
 )
 
 var (
-	token string
 	botID string
 )
 
 func main() {
-	token = "MzAwNDYxMTg0Mjg3NTA2NDMz.C8sx2g.zdC9Qig7A5pcqhk54a - bDY4h6a8"
+	token := "MzAwNDkzODY2MjY2NTI1Njk2.C8wM0A.G4rGn_3Ml8XIH1yXEC-QDGrVZW0"
 	discord, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
@@ -35,8 +33,6 @@ func main() {
 		fmt.Println("error opening connection,", err)
 		return
 	}
-	fmt.Println("I have awoken :woke:")
-
 	<-make(chan struct{})
 	return
 }
@@ -54,47 +50,17 @@ func returnTech(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 	if content == "-nixeus" {
-		printHelp()
-	}
-	xlsxFile, err := xlsx.OpenFile("C:/Users/Sean/go/src/discord_bot/stats_sheet.xlsx")
-	if err != nil {
-		fmt.Println("error opening excel file, ", err)
-		return
+		summary := ""
+		summary += "```"
+		summary += "Nixeus records and keeps track of your active positions. To have Nixeus start recording your position, use the following format:\n\n\t\"-in [ticker name] [amount of shares] [price]\"\n\nFor example, if you started a position in 10 shares of AAPL at $100 per share, you would enter: \n\n\t\"-in AAPL 10 100\"\n\nTo have Nixeus record the exiting of your position, use the following format:\n\n\t\"-out [ticker name] [price]\"\n\nFor example, if you exited your AAPL at $150 per share, you would enter:\n\n\t\"-out AAPL 150\""
+		summary += "```"
+		_, _ = s.ChannelMessageSend(m.ChannelID, summary)
 	}
 	if strings.HasPrefix(content, "-in") {
-		line := strings.Split(content, " ")
-		sheet := xlsxFile.Sheets[0]
-		row := sheet.AddRow()
-		for i := 0; i < 15; i++ {
-			row.AddCell()
-		}
-		row.Cells[0].SetString(user.Username)
-		row.Cells[1].SetString("=NOW()")
-		row.Cells[2].SetString(strings.ToUpper(line[1]))
-		row.Cells[3].SetString(line[2])
-		row.Cells[4].SetString(line[3])
+
 	}
 	if strings.HasPrefix(content, "-out") {
-		line := strings.Split(content, " ")
-		sheet := xlsxFile.Sheets[0]
-		for _, row := range sheet.Rows {
-			if row.Cells[0].Value == user.Username && row.Cells[2].Value == line[1] {
-				row.Cells[10].SetString("=NOW()")
-				row.Cells[11].SetString(line[2])
-			}
-		}
+
 	}
 
-}
-
-func printHelp() {
-	fmt.Println("Nixeus records and keeps track of your active positions")
-	fmt.Println("To have Nixeus start recording your position, use the following format:")
-	fmt.Println("\t\"-in [ticker name] [amount of shares] [price]\"")
-	fmt.Println("For example, if you started a position in 10 shares of AAPL at $100 per share, you would enter:")
-	fmt.Println("\t\"-in AAPL 10 100\"")
-	fmt.Println("To have Nixeus record the exiting of your position, use the following format:")
-	fmt.Println("\t\"-out [ticker name] [price]\"")
-	fmt.Println("For example, if you exited your AAPL at $150 per share, you would enter:")
-	fmt.Println("\t\"-out AAPL 150\"")
 }
