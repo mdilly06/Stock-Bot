bf79dafc9af354022a9290e91c24262a2dae746b
chanzer0 <seansailer28@gmail.com>
2017-04-09 00:02:06 -0500

    Started -out condition

--- main.go
+++ main.go
@@ -85,20 +85,33 @@ func returnTech(s *discordgo.Session, m *discordgo.MessageCreate) {
 		row.Cells[2].SetString(line[1])
 		row.Cells[3].SetString(line[2])
 	}
+	if strings.HasPrefix(content, "-out") {
+		line := strings.Split(content, " ")
+		sheet := xlsxFile.Sheets[0]
+		for _, row := range sheet.Rows {
+			if row.Cells[0].Value == user.Username && row.Cells[2].Value == line[1] {
+				if len(line) == 4 {
+					row.Cells[10].SetString(line[3])
+				} else {
+					row.Cells[10].SetString("=TODAY()")
+				}
+			}
+		}
+	}
 
 }
 
 func printHelp() {
 	fmt.Println("Nixeus records and keeps track of your active positions")
 	fmt.Println("To have Nixeus start recording your position, use the following format:")
-	fmt.Println("\t\"-in [ticker name] [amount of shares] [date]")
+	fmt.Println("\t\"-in [ticker name] [amount of shares] [date]\"")
 	fmt.Println("For example, if you started a position in 10 shares of AAPL on June 7th, 2014, you would enter:")
-	fmt.Println("\t\"-in AAPL 10 6/7/14")
+	fmt.Println("\t\"-in AAPL 10 6/7/14\"")
 	fmt.Println("To have Nixeus record the exiting of your position, use the following format:")
-	fmt.Println("\t\"-out [ticker name] [amount of shares] [date]")
+	fmt.Println("\t\"-out [ticker name] [amount of shares] [date]\"")
 	fmt.Println("For example, if you exited a position of 10 shares of AAPL on May 21st, 2016, you would enter:")
-	fmt.Println("\t\"-out AAPL 10 5/21/16")
-	fmt.Println("For shorthand, if you would like to use todays date for the [date], use the keyword \"today")
+	fmt.Println("\t\"-out AAPL 10 5/21/16\"")
+	fmt.Println("For shorthand, if you would like to use todays date for the [date], use the keyword \"today\"")
 	fmt.Println("An example: \"-in F 500 today\" would start recording a position of 500 shares in F on the currrent date.")
 	fmt.Println("Note that the \"today\" keyword can be used for both entering (-in) and exiting (-out) positions.")
 }
